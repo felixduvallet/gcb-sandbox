05b8096eb9524e05448231538659fede1efa9a1c
Felix Duvallet <felixd@gmail.com>
2019-01-09 17:47:45 -0800

    Add a native build job

--- run_coverage.sh
+++ run_coverage.sh
@@ -3,3 +3,9 @@
 rm -f coverage.html
 bazel coverage //source/cpp_native/...
 llvm-cov show -instr-profile=bazel-out/k8-fastbuild/testlogs/source/cpp_native/tests/multiply_tests/coverage.dat bazel-out/k8-fastbuild/bin/source/cpp_native/tests/multiply_tests -path-equivalence -use-color --format html > coverage.html
+
+# Compatible with codecov
+
+llvm-profdata merge -sparse /tmp/llvm_profile/profile-*.profraw -o coverage.profdata
+
+llvm-cov show ./hello -instr-profile=coverage.profdata > coverage.txt
