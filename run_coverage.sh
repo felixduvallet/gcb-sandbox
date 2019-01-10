e885d418823328a93af759e72594f51ce4e91bd6
Felix Duvallet <felix@kodiak.ai>
2019-01-10 07:41:39 -0800

    show coverage for multiple files

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,12 +1,40 @@
 #!/usr/bin/env bash
 
+set -x
+set -e
+
 rm -f coverage.html
+rm -f coverage.txt
 bazel coverage //source/cpp_native/...
-llvm-cov show -instr-profile=bazel-out/k8-fastbuild/testlogs/source/cpp_native/multiply/tests/multiply_tests/coverage.dat bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests -path-equivalence -use-color --format html > coverage.html
+
+
+# llvm-cov show -instr-profile=bazel-out/k8-fastbuild/testlogs/source/cpp_native/multiply/tests/multiply_tests/coverage.dat bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests -path-equivalence -use-color --format html > coverage.html
+# llvm-cov show -instr-profile=bazel-out/k8-fastbuild/testlogs/source/cpp_native/multiply/tests/multiply_tests/coverage.dat bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests -path-equivalence -use-color > coverage.txt
 
 # Compatible with codecov
 
 # llvm-profdata merge -sparse /tmp/llvm_profile/profile-*.profraw -o coverage.profdata
 
 # llvm-cov show ./hello -instr-profile=coverage.profdata > coverage.txt
-llvm-cov show -instr-profile=bazel-out/k8-fastbuild/testlogs/source/cpp_native/multiply/tests/multiply_tests/coverage.dat bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests -path-equivalence -use-color > coverage.txt
+
+
+## Merge then parse
+
+#-sparse \
+llvm-profdata merge \
+              bazel-out/k8-fastbuild/testlogs/source/cpp_native/add/tests/add_tests/coverage.dat \
+              bazel-out/k8-fastbuild/testlogs/source/cpp_native/multiply/tests/multiply_tests/coverage.dat \
+              -o aggregate.dat
+
+llvm-cov show -instr-profile=aggregate.dat \
+         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/add/tests/add_tests \
+         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests \
+         -path-equivalence -use-color > coverage.txt
+
+llvm-cov report -instr-profile=aggregate.dat \
+         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/add/tests/add_tests \
+         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests
+
+echo 'Finished'
+
+# cat coverage.txt
--- source/cpp_native/add/add.h
+++ source/cpp_native/add/add.h
@@ -3,3 +3,7 @@
 double add_two(double x) { return 2.0 + x; }
 
 double add_three(double x) { return 3.0 + x; }
+
+double add_four(double x) { return 4.0 + x; }
+
+double add_five(double x) { return 5.0 + x; }
