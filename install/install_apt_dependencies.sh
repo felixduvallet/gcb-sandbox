5ee0dcb73bcf31b848360d296355b69a8efd3956
Felix Duvallet <felix@kodiak.ai>
2019-01-10 14:14:53 -0800

    remove some unecessary dependencies

--- install/install_apt_dependencies.sh
+++ install/install_apt_dependencies.sh
@@ -8,39 +8,9 @@ sudo apt-get update && \
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
 clang \
 clang-format \
-zsh \
-llvm \
 cmake \
 curl \
 git \
-git-lfs \
 less \
-libavcodec-dev \
-libavformat-dev \
-libczmq-dev \
-libdc1394-22-dev \
-libgtk2.0-dev \
-libjpeg-dev \
-libomp-dev \
-libopencv-dev \
-libpng-dev \
-libswscale-dev \
-libtbb-dev \
-libtbb2 \
-libtiff-dev \
-libusb-1.0-0-dev \
-libzmqpp-dev \
 nano \
-nvidia-cuda-toolkit \
-pkg-config \
-pmount \
-python \
-python-dev \
-python-numpy \
-python3-dev \
-python3-pip \
-sox \
-unzip \
-zip \
-zlib1g-dev \
-setserial
+zsh
