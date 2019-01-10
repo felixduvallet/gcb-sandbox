b4969e7bc82a17410bc3fbce2b832d5a374fa058
Felix Duvallet <felix@kodiak.ai>
2019-01-10 14:17:47 -0800

    Test for +3

--- source/cpp_native/add/tests/add_tests.cpp
+++ source/cpp_native/add/tests/add_tests.cpp
@@ -3,6 +3,8 @@
 
 class AddTests : public ::testing::Test {};
 
-TEST_F(AddTests, TwoTimesTwo) { EXPECT_EQ(4.0, add_two(2.0)); }
+TEST_F(AddTests, AddTwo) { EXPECT_EQ(4.0, add_two(2.0)); }
 
-TEST_F(AddTests, ThreeTimesTwo) { EXPECT_EQ(5.0, add_two(3.0)); }
+TEST_F(AddTests, AddTwoAgain) { EXPECT_EQ(5.0, add_two(3.0)); }
+
+TEST_F(AddTests, AddThree) { EXPECT_EQ(3.0, add_three(0.0)); }
