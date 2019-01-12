d9504ac8495613080ddd11a512989fc02943cb68
Felix Duvallet <felix@kodiak.ai>
2019-01-11 23:04:29 -0500

    Specify array

--- run_coverage.sh
+++ run_coverage.sh
@@ -15,27 +15,22 @@ bazel coverage //source/cpp_native/...
 coverage_files=$(ls bazel-out/k8-fastbuild/testlogs/source/cpp_native/**/coverage.dat)
 test_files=$(ls bazel-out/k8-fastbuild/bin/source/cpp_native/**/tests/*_tests)
 
+# find bazel-bin/ -executable -type f | grep tests$ | uniq
+
 ## Merge then parse
 llvm-profdata merge \
               bazel-out/k8-fastbuild/testlogs/source/cpp_native/**/coverage.dat \
                -o aggregate.dat
 
-llvm-cov show -instr-profile=aggregate.dat \
-         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/add/tests/add_tests \
+OBJ_FILES=(-object=bazel-out/k8-fastbuild/bin/source/cpp_native/add/tests/add_tests \
          -object=bazel-out/k8-fastbuild/bin/source/cpp_native/power/tests/power_tests \
-         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests \
-         > coverage.txt
+         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests)
 
-llvm-cov show --format html -instr-profile=aggregate.dat \
-         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/add/tests/add_tests \
-         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/power/tests/power_tests \
-         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests \
-         > coverage.html
+llvm-cov show -instr-profile=aggregate.dat  ${OBJ_FILES[*]} > coverage.txt
 
-llvm-cov report -instr-profile=aggregate.dat \
-         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/add/tests/add_tests \
-         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/power/tests/power_tests \
-         -object=bazel-out/k8-fastbuild/bin/source/cpp_native/multiply/tests/multiply_tests
+llvm-cov show --format html -instr-profile=aggregate.dat  ${OBJ_FILES[*]} > coverage.html
+
+llvm-cov report -instr-profile=aggregate.dat ${OBJ_FILES[*]}
 
 echo 'Finished'
 
