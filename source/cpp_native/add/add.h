c879b2fba7f978368b8188198f5fe84d82527f31
Felix Duvallet <felixd@gmail.com>
2019-01-17 15:37:39 -0800

    Add some tests

--- source/cpp_native/add/add.h
+++ source/cpp_native/add/add.h
@@ -9,3 +9,5 @@ double add_three(double x) { return 3.0 + x; }
 double add_four(double x) { return 4.0 + x; }
 
 double add_five(double x) { return 5.0 + x; }
+
+double add_six(double x) { return 6.0 + x; }
