e4b1204bf27eae46e998ad6bdf75d70196177dde
Felix Duvallet <felixd@gmail.com>
2019-01-17 15:26:44 -0800

    Add four and five

--- source/cpp_native/add/tests/add_tests.cpp
+++ source/cpp_native/add/tests/add_tests.cpp
@@ -10,3 +10,5 @@ TEST_F(AddTests, AddTwo) { EXPECT_EQ(4.0, add_two(2.0)); }
 TEST_F(AddTests, AddTwoAgain) { EXPECT_EQ(5.0, add_two(3.0)); }
 
 TEST_F(AddTests, AddThree) { EXPECT_EQ(3.0, add_three(0.0)); }
+
+TEST_F(AddTests, AddFourAndFive) { EXPECT_EQ(9.0, add_four(add_five(0.0))); }
